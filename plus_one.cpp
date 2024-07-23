86317a512785143dd820c3fef0d9a04b2b6e29ea
IshanBhat007 <105714869+IshanBhat007@users.noreply.github.com>
2024-07-23 10:48:07 +0530

    Update plus_one.cpp

--- plus_one.cpp
+++ plus_one.cpp
@@ -1,54 +1,15 @@
-#include <map>
-#include <set>
-#include <list>
-#include <cmath>
-#include <ctime>
-#include <deque>
-#include <queue>
-#include <stack>
-#include <string>
-#include <bitset>
-#include <cstdio>
-#include <limits>
-#include <vector>
-#include <climits>
-#include <cstring>
-#include <cstdlib>
-#include <fstream>
-#include <numeric>
-#include <sstream>
-#include <iostream>
-#include <algorithm>
-#include <unordered_map>
-#include <iostream>
-#include <vector>
-using namespace std;
-
-int main() {
-    int n;
-    cin >> n;
-    vector<int> digits(n);
-    for (int i = 0; i < n; ++i) {
-        cin >> digits[i];
-    }
-    bool carry = true;
-    for (int i = n - 1; i >= 0; --i) {
-        if (carry) {
-            if (digits[i] < 9) {
-                digits[i]++;
-                carry = false;
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for(int i = digits.size() - 1; i >= 0; i--) {
+            digits[i] += 1;
+            if (digits[i] < 10) {
+                return digits;
             } else {
-                digits[i] = 0;
-            }}}
-    if (carry) {
-        digits.insert(digits.begin(), 1);
-    }
-    for (int i = 0; i < digits.size(); ++i) {
-        if (i > 0) {
-            cout << " ";
+                digits[i]= 0;
+            }
         }
-        cout << digits[i];
+        digits.insert(digits.begin(),1);
+        return digits;
     }
-    cout << endl;
-    return 0;
-}
+};
